bbdc66af632781b64d4390f5afafa70697a6c3da
Pepijn Schoen <pepijn@eliandor.com>
2016-09-17 12:11:39 +0200

    Implemented AppBuilderExtensions + tests

---
+++ AzureADBearerAuthentication/AzureADBearerAuthenticationOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Microsoft.AspNetCore.Builder
+{
+    public class AzureADBearerAuthenticationOptions
+    {
+        public string Audience { get; set; }
+        public string AzureADInstance { get; set; }
+        public string Tenant { get; set; }
+    }
+}
